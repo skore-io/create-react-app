c5f1e6ee4b62ce673e8c0906944dc8fea487ac44
Arthur Fanti <arthur@skore.io>
2016-10-28 15:20:48 -0200

    complete rules

--- config/stylelint.js
+++ config/stylelint.js
@@ -1,19 +1,54 @@
 module.exports = {
   rules: {
-    'block-no-empty': [ null, {
-      severity: 'warning'
-    } ],
+    'block-no-empty': true,
+    'block-no-single-line': true,
+    'color-named': [ 'never', {
+      igonre: ['inside-function']
+    }]
     'color-no-invalid-hex': true,
     'color-hex-case': 'upper',
+    'color-hex-length': 'short',
     'comment-empty-line-before': [ 'always', {
       ignore: ['stylelint-commands', 'between-comments']
-    } ],
-    'declaration-colon-space-after': 'always',
+    }],
+    'declaration-bang-space-after': 'never',
+    'declaration-block-no-duplicate-properties': true,
     'declaration-block-properties-order': 'alphabetical',
+    'declaration-block-semicolon-newline-after': 'always',
+    'declaration-colon-space-after': 'always',
+    'declaration-block-trailing-semicolon': 'always',
+    'declaration-no-important': [ true, {
+      severity: 'warning'
+    }]
     indentation: [ 2, {
       except: ['value']
     }],
+    'length-zero-no-unit': true,
     'max-empty-lines': 1,
-    'unit-whitelist': ['px', 'vh', 'vw', 's', 'deg']
+    'no-missing-end-of-source-newline': true,
+    'number-leading-zero': 'always',
+    'number-no-trailing-zeros': true,
+    'rule-non-nested-empty-line-before': 'always',
+    'selector-attribute-brackets-space-inside': 'never',
+    'selector-attribute-quotes': 'always',
+    'selector-class-pattern': '[a-z-]+',
+    'selector-combinator-space-after': 'always',
+    'selector-combinator-space-before': 'always',
+    'selector-descendant-combinator-no-non-space': true,
+    'selector-list-comma-newline-after': 'always',
+    'selector-list-comma-space-after': 'always',
+    'selector-list-comma-space-before': 'never',
+    'selector-no-id': true,
+    'selector-pseudo-class-case': 'lower',
+    'selector-pseudo-class-no-unknown': true,
+    'selector-pseudo-element-case': 'lower',
+    'selector-type-case': 'lower',
+    'selector-type-no-unknown': true,
+    'shorthand-property-no-redundant-values': true,
+    'string-quotes': 'single',
+    'unit-case': 'lower',
+    'unit-whitelist': ['px', 'vh', 'vw', 's', 'deg'],
+    'value-keyword-case': 'lower',
+    'value-no-vendor-prefix': true
   }
 }
